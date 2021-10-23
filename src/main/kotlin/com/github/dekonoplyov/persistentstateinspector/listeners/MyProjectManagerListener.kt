4a12e4f78764a1bd4d241a7fced071f43ccf1d28
GitHub Action <action@github.com>
2021-10-23 17:08:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/dekonoplyov/persistentstateinspector/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/dekonoplyov/persistentstateinspector/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.dekonoplyov.persistentstateinspector.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.dekonoplyov.persistentstateinspector.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/dekonoplyov/persistentstateinspector/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dekonoplyov.persistentstateinspector.services
+
+import com.github.dekonoplyov.persistentstateinspector.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
